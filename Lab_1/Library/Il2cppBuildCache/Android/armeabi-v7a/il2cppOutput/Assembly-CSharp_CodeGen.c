181dac3e85e547c024d5603b8ff1667607c62220
MichailAzarov <azarov1211@gmail.com>
2023-03-24 18:09:53 +0700

    Лабораторная
    Добавлена Лабораторная #1

---
+++ Lab_1/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,67 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Date::Start()
+extern void Date_Start_m092FB3DDE94A09F1F06B26AA4A677EA304ADE44B (void);
+// 0x00000002 System.Void Date::.ctor()
+extern void Date__ctor_m23170DF108AFCA9B7FFCBBC56219B988930F2DB3 (void);
+// 0x00000003 System.Void InputText::Update()
+extern void InputText_Update_m570BC8B32A5BDB59A2FDA4A9F55FAD87BB9DB53D (void);
+// 0x00000004 System.Void InputText::.ctor()
+extern void InputText__ctor_m3BC4B25FE26CDB1BAA92BEA07D5E780CA4C519F9 (void);
+// 0x00000005 System.Void SwithPhoto::DropdownToPhoto(System.Int32)
+extern void SwithPhoto_DropdownToPhoto_m32F03FEF2FFC9A4A72D6209312924F37A5CB978C (void);
+// 0x00000006 System.Void SwithPhoto::.ctor()
+extern void SwithPhoto__ctor_m9468698502C09476AA951BB00DD8B42DB6316F9A (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	Date_Start_m092FB3DDE94A09F1F06B26AA4A677EA304ADE44B,
+	Date__ctor_m23170DF108AFCA9B7FFCBBC56219B988930F2DB3,
+	InputText_Update_m570BC8B32A5BDB59A2FDA4A9F55FAD87BB9DB53D,
+	InputText__ctor_m3BC4B25FE26CDB1BAA92BEA07D5E780CA4C519F9,
+	SwithPhoto_DropdownToPhoto_m32F03FEF2FFC9A4A72D6209312924F37A5CB978C,
+	SwithPhoto__ctor_m9468698502C09476AA951BB00DD8B42DB6316F9A,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	1111,
+	1111,
+	1111,
+	1111,
+	944,
+	1111,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
